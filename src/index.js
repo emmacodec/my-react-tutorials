24e8baca773211367e261dc282243290235276b8
Emmacodec <danielemmanuel7553@gmail.com>
2023-04-20 00:33:24 +0100

    wee

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 // write some react code
+import React, {component} from 'react';
+import {render} from 'react-dom';
+const node=document.getElementById('root');
+const root=
+  React.createElement('div',{},//
+   React.createElement('h1',{},"Hello world!",//
+    React.createElement('a', {href: 'mailto:github.com/emmacodec'},
+     React.createElement('h1',{},"React in Action"),
+      React.createElement('em',{}, "...and now it really is!")
+    )
+   )
+  );
+  render(root,node); //
